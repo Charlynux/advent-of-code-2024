f8dc7519cd72416349650b0bda326b5806635c9a
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2024-12-04 06:51:41 +0100

    Jour 3 - Partie 2

--- src/advent_2024/day03.ml
+++ src/advent_2024/day03.ml
@@ -14,7 +14,7 @@ let find_mults s =
       let a = int_of_string(Str.matched_group 1 s)
       and b = int_of_string(Str.matched_group 2 s) in
       loop ((a, b)::acc) (m + 1)
-    with
+    With
       Not_found -> acc in
   loop [] 0;;
 
@@ -27,3 +27,53 @@ let calculate_line s = s |> find_mults |> List.map mult |> list_sum;;
 read_lines ("../../data/day03.input")
 |> List.map calculate_line
 |> list_sum;;
+
+let example_input2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";;
+
+let part2_regex = Str.regexp "\\(don't()\\|do()\\|mul(\\([0-9]+\\),\\([0-9]+\\))\\)";;
+
+type op =
+  | Do
+  | Dont
+  | Mult of int * int;;
+
+let find_ops s =
+  let rec loop n =
+    try
+      let next = (Str.search_forward part2_regex s n) + 1 in
+      let found = Str.matched_group 1 s in
+      match (found) with
+        "do()" -> Do::(loop next)
+      | "don't()" -> Dont::(loop next)
+      | _ ->
+         let a = int_of_string(Str.matched_group 2 s)
+         and b = int_of_string(Str.matched_group 3 s) in
+         Mult(a,b)::(loop next)
+    with
+      Not_found -> [] in
+  loop 0;;
+
+find_ops example_input2;;
+
+let rec aggregate_ops enabled xs =
+  match (xs) with
+    [] -> []
+  | Do::tl -> aggregate_ops true tl
+  | Dont::tl -> aggregate_ops false tl
+  | Mult(a,b)::tl -> if enabled then
+                       (a*b)::(aggregate_ops enabled tl)
+                     else
+                       aggregate_ops enabled tl;;
+
+aggregate_ops true (find_ops example_input2);;
+
+read_lines ("../../data/day03.input")
+|> List.concat_map find_ops
+|> (aggregate_ops true)
+|> list_sum;;
+(**
+   69247082 - too high
+   Je traitais les lignes une à une, alors que le statut "enabled" doit être conservé de ligne en ligne.
+
+   63013756 - Correct
+ *)
