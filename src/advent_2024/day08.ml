e481cfaa8059e2413af9f9439e05a0dfdd21a665
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2024-12-08 09:04:50 +0100

    Jour 8 - Partie 1

--- src/advent_2024/day08.ml
+++ src/advent_2024/day08.ml
@@ -34,10 +34,53 @@ let parse_input lines =
                         (parse_line acc y line)
                         (y + 1)
                         rest in
-  loop
-    []
-    0
-    lines;;
+  ((List.length lines, String.length (List.hd lines)),
+   loop [] 0 lines);;
 
 let example_input = parse_input (read_lines "../../data/day08-example.input");;
 parse_input (read_lines "../../data/day08.input");;
+
+let find_antinodes (x, y) (x', y') =
+  let offset_x = x' - x
+  and offset_y = y' - y in
+  [
+    ((x - offset_x),(y - offset_y));
+    ((x' + offset_x),(y' + offset_y))
+  ];;
+
+find_antinodes (5, 5) (4, 3);;
+(* (3, 1) (6, 7)*)
+
+let rec pairing_antennas = function
+    [] -> []
+  | [a] -> []
+  | a :: tl -> (List.concat_map (find_antinodes a) tl)
+               @(pairing_antennas tl);;
+
+pairing_antennas [(5, 5);(4, 3);(8,4)];;
+
+let is_inbound (max_x, max_y) (x, y) =
+ (x >= 0) && (x < max_x) && (y >= 0) && (y < max_y);;
+
+let compare_points (x0,y0) (x1,y1) =
+  match Stdlib.compare x0 x1 with
+    0 -> Stdlib.compare y0 y1
+  | c -> c;;
+
+let solve_part1 (dimensions, antennas) =
+  antennas
+  |> List.map snd
+  |> List.concat_map pairing_antennas
+  |> List.filter (is_inbound dimensions)
+  |> List.sort_uniq compare_points;;
+
+(** Comparaison des résultats de l'exemple *)
+"../../data/day08-example-solution.input"
+|> read_lines |> parse_input |> snd |> (List.assoc '#')
+|> (List.sort compare_points);;
+
+solve_part1 example_input |> List.length;;
+
+parse_input (read_lines "../../data/day08.input")
+|> solve_part1
+|> List.length;;
